0e09b1055023c538221f1becf53700c6931e257e
praveen-47 <hpraveenmatheesha@gmail.com>
2021-06-12 04:34:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React ,{useEffect,useState} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+// import { useEffect } from 'react';
+// import { response } from 'express';
+// const url ='https://www.googleapis.com/books/v1/volumes?q=reactjs'
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const apiGet = async() => {
+    await fetch("https://www.googleapis.com/books/v1/volumes?q=reactjs")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setData(json);
+        console.log(data.items)
+      });
+  };
+ 
+  // useEffect(()=>{
+  //   // piGet();a
+  // })
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={apiGet}>Fetch API</button>
+      
+      <div>
+        <ul>
+          {data.items.map((item) => (
+            <li key={item.id}>
+             {item.volumeInfo.authors},
+             {item.volumeInfo.publisher}, 
+             {item.volumeInfo.publishedDate},
+             {item.volumeInfo.description},
+
+
+
+           </li>
+            
+          ))}
+
+        </ul>
+        <h1>brake</h1>
+        <ul>
+          {data.items.map((item)=>(
+            <li key={item.key}>
+            {item.volumeInfo.previewLink}
+            </li>
+          ))}
+        </ul>
+        {/* <img src={item.volumeInfo.imageLinks.smallThumbnail} alt="" /> */}
+      </div>
+      
     </div>
   );
 }
